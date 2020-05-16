ddd2966dd5e1d2bc98fafd376d5bf803dff019f9
Brian Arango <brianvictus@gmail.com>
2020-05-16 18:21:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,14 +2,9 @@ def valid_move?(board, index)
   !position_taken?(board, index) && (index).between?(0,8)
 end
 
-#def valid_move?(board, index)
- #  if index.between?(0,8)
-  #   if !position_taken?(board, index)
-   #    true
-    # end
-   #end
-#end
-
+def position_taken?(board, index)
+  !(board[index] == "" || board[index] == " " || board[index] == nil)
+end
 
 
 
